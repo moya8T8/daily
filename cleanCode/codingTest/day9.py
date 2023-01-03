3e022dc6b4c3119578852e62f3a06749c6173c4e
moya <moyamoyaa88@gmail.com>
2023-01-03 22:37:42 +0900

    3일

---
+++ cleanCode/codingTest/day9.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    divisorsList = []
+
+    for i in range(1, n + 1):
+        if (n % i == 0) :
+            divisorsList.append(i)
+        
+    for i in divisorsList:
+        answer += i
+    return answer
+
