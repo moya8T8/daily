1b226127a332334a9b992013cb2022e46ca9e978
moya <moyamoyaa88@gmail.com>
2023-01-13 00:25:21 +0900

    13일

--- cleanCode/codingTest/day6.py
+++ cleanCode/codingTest/day6.py
@@ -1,4 +1,4 @@
-def solution(my_string):
+def solution1(my_string):
     answer = ''
     for i in range(len(my_string)):
         answer += my_string[-1-i]
@@ -7,3 +7,15 @@ def solution(my_string):
 n = int(input())
 for i in range(n):
     print('*' * (i+1)  )
+
+def solution2(num_list):
+    answer = []
+    a = 0
+    b = 0
+    for num in num_list:
+        if(num % 2 == 0):
+            a +=1
+        else :
+            b +=1
+    answer = [a,b]
+    return answer
