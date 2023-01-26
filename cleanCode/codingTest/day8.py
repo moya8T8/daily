949f6d2db8c9d7ad573c0c34faa84b0c75bd8ebc
moya <moyamoyaa88@gmail.com>
2023-01-26 22:49:06 +0900

    26일

--- cleanCode/codingTest/day8.py
+++ cleanCode/codingTest/day8.py
@@ -15,3 +15,30 @@ def solution(numbers, num1, num2):
         answer.append(numbers[i])   
     print(answer)
     return answer
+
+def solution(age):
+    answer = ''
+    age = str(age)
+    for i in range(len(age)):
+        if(age[i]=='0'):
+            answer += 'a'
+        if(age[i]=='1'):
+            answer += 'b'
+        if(age[i]=='2'):
+            answer += 'c'
+        if(age[i]=='3'):
+            answer += 'd'
+        if(age[i]=='4'):
+            answer += 'e'
+        if(age[i]=='5'):
+            answer += 'f'
+        if(age[i]=='6'):
+            answer += 'g'
+        if(age[i]=='7'):
+            answer += 'h'
+        if(age[i]=='8'):
+            answer += 'i'
+        if(age[i]=='9'):
+            answer += 'k'
+    print(answer)
+    return answer
