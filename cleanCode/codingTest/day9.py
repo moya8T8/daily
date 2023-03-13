5948444df4f9ff3c880fa7098b436f01f6088e56
moya <moyamoyaa88@gmail.com>
2023-03-13 23:22:31 +0900

    13일

--- cleanCode/codingTest/day9.py
+++ cleanCode/codingTest/day9.py
@@ -12,4 +12,42 @@ def solution(n):
         answer += i
     return answer
 
-       
+#개미군단  1차시도 
+def solution(hp):
+    jangun = 5
+    beong = 3
+    yil = 1
+
+    sanyagam = hp
+    answer = 0
+    if(sanyagam % jangun > 0):
+        abc = sanyagam // jangun
+        answer += abc
+        sanyagam = sanyagam - (abc * jangun)
+        if(sanyagam % beong > 0):
+            ef = sanyagam // beong
+            answer += ef
+            sanyagam = sanyagam -(ef * beong)
+            if(sanyagam != 0):
+                answer += sanyagam
+        else :
+            ef = sanyagam // beong
+            answer += ef
+    else :
+        abc = sanyagam // jangun
+        answer += abc
+    print(answer)
+
+    return answer
+
+
+#2차시도 
+def solution(hp):
+    answer = 0
+    answer += hp//5
+    hp %= 5
+    answer += hp//3
+    hp %= 3
+    answer += hp//1
+
+    return answer
